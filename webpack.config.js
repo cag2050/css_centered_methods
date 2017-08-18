8da0a51e436a9faed5484c5b9a2ea73e2b82673e
cag2050 <cag2050@163.com>
2017-08-18 17:36:48 +0800

    m

--- webpack.config.js
+++ webpack.config.js
@@ -36,11 +36,11 @@ var config = {
         loaders: [
             {
                 test: /\.css$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+                loader: ExtractTextPlugin.extract({fallback: "style-loader", use: "css-loader"})
             },
             {
                 test: /\.scss$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+                loader: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader!sass-loader'})
             },
             {test: /\.(png|jpg)$/, loader: 'url-loader'},
             {
